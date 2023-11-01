195884883ea14dd9eb7a9b0023898c584917c4b9
JohnnyM1ng <1290826968@qq.com>
2023-11-01 19:21:13 +0800

    add new practices

--- git_test/src/Tutorials/ArrayDemo.java
+++ git_test/src/Tutorials/ArrayDemo.java
@@ -2,8 +2,20 @@ package Tutorials;
 
 public class ArrayDemo {
     public static void main(String[] args) {
-        int[] anArray = new int[10];
-        anArray = new int[10];
+//
+        String[] copyFrom = {
+                "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
+                "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",
+                "Marocchino", "Ristretto" };
+
+        String[] copyTo = {
+                "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
+                "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",
+                "Marocchino", "Ristretto" };
+        System.arraycopy(copyFrom, 0, copyTo, 3, 7);
+        for (String coffee : copyTo) {
+            System.out.print(coffee + " ");
+        }
 
     }
 }
---
+++ git_test/src/Tutorials/OperationDemo/ConditionalOperators.java
@@ -0,0 +1,13 @@
+package Tutorials.OperationDemo;
+
+public class ConditionalOperators {
+    public static void main(String[] args){
+        int value1 = 1;
+        int value2 = 2;
+        int result;
+        boolean someCondition = true;
+        result = someCondition ? value1 : value2;
+
+        System.out.println(result);
+    }
+}
